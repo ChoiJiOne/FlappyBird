88fa2a5804c5796617fc0eabf7fc9c79a48de97c
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-16 12:11:21 +0900

    FlappyBird 프로젝트 초기화

--- FlappyBird/Src/BoundCircle2D.cpp
+++
@@ -1,9 +0,0 @@
-#include "MathModule.h"
-
-#include "Assertion.h"
-#include "BoundCircle2D.h"
-
-bool BoundCircle2D::Intersect(const IBound2D* bound) const
-{
-	return IBound2D::IsIntersect(this, bound);
-}
--- Package_RelWithDebInfo.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-SET mode=RelWithDebInfo
-SET script=Script\Package.bat
-
-%script% %mode%
