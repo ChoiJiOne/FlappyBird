c9acadd9b5de5e09d8b06aef5f0e82130a6bf008
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-16 13:24:54 +0900

    메인 루프에 백버퍼를 흰색으로 초기화하는 기능 추가
    - 윈도우 크기 및 타이틀을 상수로 설정

--- FlappyBird/Source/Main.cs
+++ FlappyBird/Source/Main.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using Raylib_cs;
 
 class GameApp
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Raylib.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+
+        while(!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RayWhite);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
     }
+
+    private static readonly int WINDOW_WIDTH = 600;
+    private static readonly int WINDOW_HEIGHT = 800;
+    private static readonly string WINDOW_TITLE = "FlappyBird";
 }
